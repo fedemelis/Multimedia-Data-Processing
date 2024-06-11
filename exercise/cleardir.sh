08ec5b9e4f243fb50df0fbb5d6177cdf8a19f498
Federico Melis <70884674+fedemelis@users.noreply.github.com>
2024-06-11 10:27:34 +0200

    removing test case files

---
+++ exercise/cleardir.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Function to create the 'data' directory if it doesn't exist
+create_data_directory() {
+    local dir="$1/data"
+    if [[ ! -d "$dir" ]]; then
+        mkdir -p "$dir"
+    fi
+}
+
+# Function to move specified file types to 'data' directory
+move_files() {
+    local dir="$1"
+    local data_dir="$dir/data"
+    
+    create_data_directory "$dir"
+    
+    # Moving specified file types to 'data' directory
+    mv "$dir"/*.pam "$dir"/*.txt "$dir"/*.ppm "$dir"/*.hdr "$dir"/*.pmg "$dir"/*.huffdiff "$dir"/*.lz4 "$dir"/*.torrent "$data_dir" 2>/dev/null
+}
+
+# Main function
+main() {
+    local current_dir
+    
+    find . -type d | while read -r current_dir; do
+        move_files "$current_dir"
+    done
+}
+
+main "$@"
+
+
+
+come lo lancio?
+
+bash
+Copia codice
+#!/bin/bash
+
+# Function to create the 'data' directory if it doesn't exist
+create_data_directory() {
+    local dir="$1/data"
+    if [[ ! -d "$dir" ]]; then
+        mkdir -p "$dir"
+    fi
+}
+
+# Function to move specified file types to 'data' directory
+move_files() {
+    local dir="$1"
+    local data_dir="$dir/data"
+    
+    create_data_directory "$dir"
+    
+    # Moving specified file types to 'data' directory
+    mv "$dir"/*.pam "$dir"/*.txt "$dir"/*.ppm "$dir"/*.hdr "$dir"/*.pmg "$dir"/*.huffdiff "$dir"/*.lz4 "$dir"/*.torrent "$data_dir" 2>/dev/null
+}
+
+# Main function
+main() {
+    local current_dir
+    
+    find . -type d | while read -r current_dir; do
+        move_files "$current_dir"
+    done
+}
+
+main "$@"
--- exercise/tescpp/tescpp/main.cpp
+++ exercise/tescpp/tescpp/main.cpp
@@ -1,29 +1,79 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstdint>
+#include <fstream>
 
-struct vector{
-
-	size_t size;
-	size_t len;
-	int* num;
-
-	vector() {
-		
-		size = 10;
-		len = 0;
-		num = (int*)malloc(size * sizeof(int));
+struct bitreader
+{
+	uint8_t buffer_;
+	size_t n_ = 0;
+	std::ifstream& is_;
+	
+	uint8_t readbit() {
+		if (n_ == 0)
+		{
+			buffer_ = is_.get();
+			n_ = 8;
+		}
+		--n_;
+		return (buffer_ >> n_) & 1;
 	}
 
-	int operator[](int i){
-		return num[i];
+	bitreader(std::ifstream& is) : is_(is) {}
+
+	uint64_t operator()(int numbits) {
+		uint64_t val = 0;
+		for (int i = numbits - 1; i >= 0; i--)
+		{
+			val <<= 1;
+			val |= readbit();
+		}
+		return val;
 	}
-
-	~vector() {
-		free(num);
-	}
-
-	//push back
-
-
-
 };
+
+
+struct bitwriter
+{
+	uint8_t buffer_;
+	size_t n_ = 0;
+	std::ofstream& os_;
+
+	void writebit(uint8_t bit) {
+		buffer_ <<= 1;
+		buffer_ |= bit;
+		++n_;
+		if (n_ == 8)
+		{
+			os_.put(buffer_);
+			n_ = 0;
+		}
+	}
+
+	bitwriter(std::ofstream& os) : os_(os) {}
+
+	~bitwriter() {
+		flush();
+	}
+
+	void operator()(uint64_t val, int numbits) {
+		for (int i = numbits - 1; i >= 0; i--)
+		{
+			writebit((val >> i) & 1);
+		}
+	}
+
+	void flush() {
+		while (n_ != 0)
+		{
+			writebit(0);
+		}
+	}
+};
+
+
+int main(int argc, char** argv) {
+	
+
+
+}
